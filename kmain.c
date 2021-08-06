2051bfe51fd7d7a6457a0b4d50eb9916899b8d43
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2021-08-07 01:45:46 +0530

    Added functions

--- kmain.c
+++ kmain.c
@@ -1,4 +1,17 @@
+#include "io.h"
+#include "fb.h"
+#include "serial_port.h"
+
+    /* The C function */
     int sum_of_three(int arg1, int arg2, int arg3)
     {
         return arg1 + arg2 + arg3;
     }
+    
+    void run(){
+        char c[] = "Welcome";
+        write(c, 9);
+
+        serial_write(SERIAL_COM1_BASE, c, 7);
+    }
+    
