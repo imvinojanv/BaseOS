a2d394095b3e6c0464328d75f0129145a87b8a78
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2021-08-07 01:46:44 +0530

    some changes on loader.s

--- loader.s
+++ loader.s
@@ -4,13 +4,13 @@
     FLAGS        equ 0x0            ; multiboot flags
     CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
-    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
-
+                                    
+    KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
     section .bss
-    align 4                         ; align at 4 bytes
-    kernel_stack:                   ; label points to beginning of memory
-        resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
-
+    align 4                                     ; align at 4 bytes
+    kernel_stack:                               ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
+        
     section .text:                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
@@ -18,14 +18,13 @@
         dd CHECKSUM                 ; and the checksum
 
     loader:                         ; the loader label (defined as entry point in linker script)
-        mov esp, kernel_stack + KERNEL_STACK_SIZE       ; point esp to the start of the
-                                                                                        ;stack (end of memory area)
-        ; The assembly code
-        extern sum_of_three   ; the function sum_of_three is defined elsewhere
-        push dword 3            ; arg3
-        push dword 2            ; arg2
-        push dword 1            ; arg1
-        call sum_of_three       ; call the function, the result will be in eax
+        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+        mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+                                                ; stack (end of memory area)
+                                                
+        ; Calling C code
+        extern run
+        call run        
 
     .loop:
         jmp .loop                   ; loop forever
