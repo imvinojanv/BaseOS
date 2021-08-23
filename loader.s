7a86e7b24dbc65c8aeb608230b5bfc06e40d4a9b
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2021-08-23 15:43:30 +0530

    Update loader.s file

--- loader.s
+++ loader.s
@@ -5,26 +5,27 @@
     CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
                                     
-    KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
+    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+
     section .bss
-    align 4                                     ; align at 4 bytes
-    kernel_stack:                               ; label points to beginning of memory
-        resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
-        
-    section .text:                  ; start of the text (code) section
+    align 4                         ; align at 4 bytes
+    kernel_stack:                   ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
+
+    section .text             ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
         dd FLAGS                    ; the flags,
         dd CHECKSUM                 ; and the checksum
 
-    loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
-        mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
-                                                ; stack (end of memory area)
-                                                
-        ; Calling C code
-        extern run
-        call run        
-
+    loader:                         			; the loader label 
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   	; point esp to the start of the
+                                                	; stack (end of memory area)
+        
+        
+    extern kmain   		           ; the main function is defined elsewhere
+    call kmain                             ; call the function, the result will be in eax
+    
+    
     .loop:
         jmp .loop                   ; loop forever
