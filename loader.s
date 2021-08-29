e122cfea78839208cbe4d87b78540e03dfc2a7ad
Vinojan Veerapathrathasan <48822560+Vinojan1999@users.noreply.github.com>
2021-08-29 11:31:11 +0530

    Integrate user modes (#6)
    * Created a file
    
    * Added call function in c
    
    * Updated to instruct GRUB how to load our modules
    
    * Updated the module file to make GRUB
    
    * Added code to program.s

--- loader.s
+++ loader.s
@@ -1,31 +1,34 @@
     global loader                   ; the entry symbol for ELF
-
-    MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-    FLAGS        equ 0x0            ; multiboot flags
-    CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
-                                    ; (magic number + checksum + flags should equal 0)
-                                    
+    extern kmain   	             ; the function kmain is defined elsewhere
+    
     KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
-
+    			
+    MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
+    ALIGN_MODULES equ 0x00000001    ; tell GRUB to align modules
+    CHECKSUM     equ -(MAGIC_NUMBER + ALIGN_MODULES )  
+    				     ; calculate the checksum
+                                    ; (magic number + checksum + flags should equal 0)
+    
     section .bss
     align 4                         ; align at 4 bytes
     kernel_stack:                   ; label points to beginning of memory
         resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
-
-    section .text             ; start of the text (code) section
+    
+        
+    section .text                   ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
-        dd FLAGS                    ; the flags,
-        dd CHECKSUM                 ; and the checksum
-
-    loader:                         			; the loader label 
-    mov esp, kernel_stack + KERNEL_STACK_SIZE   	; point esp to the start of the
-                                                	; stack (end of memory area)
-        
-        
-    extern kmain   		           ; the main function is defined elsewhere
-    call kmain                             ; call the function, the result will be in eax
+        dd ALIGN_MODULES            ; write the align modules instruction
+        dd CHECKSUM                 ; write the checksum
     
+    mov esp, kernel_stack + KERNEL_STACK_SIZE       ; point esp to the start of the stack (end of memory area)
     
+    loader:                         ; the loader label (defined as entry point in linker script)
+	
+    	push ebx                    ; multiboot info in ebx 
+    	call kmain                  ; call the function, the result will be in eax
+	
     .loop:
         jmp .loop                   ; loop forever
+        
+            
