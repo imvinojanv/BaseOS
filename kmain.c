50791dba5bd3680ec8f66f5ac4ff7ee06170545c
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2021-09-07 10:15:12 +0530

    Added called paging function

--- kmain.c
+++ kmain.c
@@ -1,39 +1,36 @@
-#include "serial_port.c"
+#include "serial_port.h"
 #include "memory_segments.h"
 #include "interrupts.h"
 #include "multiboot.h"
+#include "paging.h"
 
-
-void init() {
-  
-  init_gdt();
-  serial_configure(SERIAL_COM1_BASE, Baud_115200);
-  interrupts_install_idt();
+void init(){
+   segments_install_gdt();
+   interrupts_install_idt();
+   init_paging();
 }
 
-
-
 int kmain(unsigned int ebx){
-    	init();
-    	
-  	multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
-  	multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
-  	unsigned int address_of_module = modules->mod_start;
-  	
-  	if((mbinfo->mods_count) == 1){
-  		char message[] = "ONE module loaded successfully!";
-  		serial_write(0x3F8,message,sizeof(message));
-  		
-  		typedef void (*call_module_t)(void);
-        	
-        	call_module_t start_program = (call_module_t) address_of_module;
-        	start_program();
-        	/* we'll never get here, unless the module code returns */
+   
+   init();
 
-  	}else{
-  		char message[] = "Error: More than ONE module loaded";
-  		serial_write(0x3F8,message,sizeof(message));
-  	}
-  	 
-  	return 0;
+   multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+   multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
+   unsigned int address_of_module = modules->mod_start;
+
+   if((mbinfo->mods_count) == 1){
+      char str[] = "Module successfully loaded\n";
+      serial_write(str,sizeof(str));
+      
+      typedef void (*call_module_t)(void);
+         call_module_t start_program = (call_module_t) address_of_module;
+         start_program();
+
+   }
+   else{
+      char str[] = "Multiple modules loaded\n";
+      serial_write(str,sizeof(str));
+   }
+
+   return 0;
 }
